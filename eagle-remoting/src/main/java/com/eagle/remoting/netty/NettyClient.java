e8c921dc7254a6065df547775feab81ee518bd55
Kevin <wb_chenhai@zhongan.com>
2018-09-11 16:46:33 +0800

    netty support

---
+++ eagle-remoting/src/main/java/com/eagle/remoting/netty/NettyClient.java
@@ -0,0 +1,7 @@
+package com.eagle.remoting.netty;
+
+/**
+ * Created by Kevin on 2018/9/10.
+ */
+public class NettyClient {
+}
---
+++ eagle-remoting/src/main/java/com/eagle/remoting/netty/NettyClientHandler.java
@@ -0,0 +1,7 @@
+package com.eagle.remoting.netty;
+
+/**
+ * Created by Kevin on 2018/9/11.
+ */
+public class NettyClientHandler {
+}
